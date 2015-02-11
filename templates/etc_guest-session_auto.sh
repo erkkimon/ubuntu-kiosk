d620fab4702fb1a2a418b6b3e184a93959b90736
Tom Himanen <tom.himanen@gmail.com>
2015-02-11 23:18:27 +0200

    uri-fix for bg

--- templates/etc_guest-session_auto.sh
+++ templates/etc_guest-session_auto.sh
@@ -1,3 +1,3 @@
-gsettings set org.gnome.desktop.background picture-uri 'file:///usr/share/backgrounds/bg.jpg'
+gsettings set org.gnome.desktop.background picture-uri 'file:///usr/share/backgrounds/kiosk/bg.jpg'
 mkdir ~/asdf
 # gsettings set org.gnome.desktop.background picture-uri file://$(find DIR -type f | shuf -n1`) # Random bg form the folder
