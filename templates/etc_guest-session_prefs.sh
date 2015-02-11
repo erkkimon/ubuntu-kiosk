4d6578cfcf178f0cd95e25365d122278db8b36d2
Tom Himanen <tom.himanen@gmail.com>
2015-02-11 22:51:11 +0200

    Background implemented for testing

--- templates/etc_guest-session_prefs.sh
+++ templates/etc_guest-session_prefs.sh
@@ -1,1 +1,2 @@
-dconf write /org/gnome/shell/favorite-apps "['firefox.desktop', 'nautilus.desktop']"
+gsettings get org.gnome.desktop.background picture-uri 'file:///usr/share/backgrounds/bg.jpg'
+# gsettings set org.gnome.desktop.background picture-uri file://$(find DIR -type f | shuf -n1`) # Random bg form the folder
