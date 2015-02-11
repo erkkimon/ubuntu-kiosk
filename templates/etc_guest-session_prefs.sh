96c439bf471569e7b6a4dfcafde2d4fb15032649
Tom Himanen <tom.himanen@gmail.com>
2015-02-11 23:05:17 +0200

    Debugging stuff

--- templates/etc_guest-session_prefs.sh
+++ templates/etc_guest-session_prefs.sh
@@ -1,2 +1,3 @@
 gsettings set org.gnome.desktop.background picture-uri 'file:///usr/share/backgrounds/bg.jpg'
+mkdir ~/asdf
 # gsettings set org.gnome.desktop.background picture-uri file://$(find DIR -type f | shuf -n1`) # Random bg form the folder
